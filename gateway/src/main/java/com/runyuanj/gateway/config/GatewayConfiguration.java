011dcf97a36ad056010436bcd84f6a608b97b221
runyuanj <wangrunyuanj@163.com>
2020-12-15 14:45:40 +0800

    稳定版本-S1

--- gateway/src/main/java/com/runyuanj/gateway/config/GatewayConfiguration.java
+++ gateway/src/main/java/com/runyuanj/gateway/config/GatewayConfiguration.java
@@ -1,9 +1,24 @@
 package com.runyuanj.gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfiguration {
+
+    /**
+     * 路由配置代码版
+     * @param builder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/login.html")
+                        .uri("localhost:8001/login"))
+                .build();
+    }
 
 }
