5c569f5c9d93fcb60ed4b22ed8f7fc7df421f617
runyuanj <wangrunyuanj@163.com>
2020-12-15 13:38:11 +0800

    稳定版本-S1

---
+++ gateway/src/main/java/com/runyuanj/gateway/config/GatewayConfiguration.java
@@ -0,0 +1,9 @@
+package com.runyuanj.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+}
