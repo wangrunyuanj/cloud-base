452ba7288390a01efa3f6e5e54a62f21abd7d165
ryj111 <57824123+wangrunyuanj@users.noreply.github.com>
2021-04-27 13:43:17 +0800

    update

--- core/src/main/java/com/runyuanj/core/processor/MultiRequestBodyArgumentProcessor.java
+++ core/src/main/java/com/runyuanj/core/processor/MultiRequestBodyArgumentProcessor.java
@@ -1,66 +1,57 @@
 package com.runyuanj.core.processor;
 
-import org.apache.commons.io.IOUtils;
-import org.springframework.core.MethodParameter;
-import org.springframework.web.bind.support.WebDataBinderFactory;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.support.ModelAndViewContainer;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
 @Deprecated
 public class MultiRequestBodyArgumentProcessor {
-    /**
-     * Whether the given {@linkplain MethodParameter method parameter} is
-     * supported by this resolver.
-     *
-     * @param parameter the method parameter to check
-     * @return {@code true} if this resolver supports the supplied parameter;
-     * {@code false} otherwise
-     */
-    //@Override
-    public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(MultiRequestBody.class);
-    }
-
-    /**
-     * Resolves a method parameter into an argument value from a given request.
-     * A {@link ModelAndViewContainer} provides access to the model for the
-     * request. A {@link WebDataBinderFactory} provides a way to create
-     * a {@link WebDataBinder} instance when needed for data binding and
-     * type conversion purposes.
-     *
-     * @param parameter     the method parameter to resolve. This parameter must
-     *                      have previously been passed to {@link #supportsParameter} which must
-     *                      have returned {@code true}.
-     * @param mavContainer  the ModelAndViewContainer for the current request
-     * @param webRequest    the current request
-     * @param binderFactory a factory for creating {@link WebDataBinder} instances
-     * @return the resolved argument value, or {@code null} if not resolvable
-     * @throws Exception in case of errors with the preparation of argument values
-     */
-    //@Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String jsonBody = this.getRequestBody(webRequest);
-
-        return null;
-    }
-
-    private String getRequestBody(NativeWebRequest webRequest) {
-        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
-
-        String jsonRequestBody = (String) webRequest.getAttribute("JSON_REQUEST_BODY", 0);
-        if (jsonRequestBody == null) {
-            try {
-                jsonRequestBody = IOUtils.toString(servletRequest.getReader());
-                webRequest.setAttribute("JSON_REQUEST_BODY", jsonRequestBody, 0);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-
-        return null;
-    }
+//    /**
+//     * Whether the given {@linkplain MethodParameter method parameter} is
+//     * supported by this resolver.
+//     *
+//     * @param parameter the method parameter to check
+//     * @return {@code true} if this resolver supports the supplied parameter;
+//     * {@code false} otherwise
+//     */
+//    //@Override
+//    public boolean supportsParameter(MethodParameter parameter) {
+//        return parameter.hasParameterAnnotation(MultiRequestBody.class);
+//    }
+//
+//    /**
+//     * Resolves a method parameter into an argument value from a given request.
+//     * A {@link ModelAndViewContainer} provides access to the model for the
+//     * request. A {@link WebDataBinderFactory} provides a way to create
+//     * a {@link WebDataBinder} instance when needed for data binding and
+//     * type conversion purposes.
+//     *
+//     * @param parameter     the method parameter to resolve. This parameter must
+//     *                      have previously been passed to {@link #supportsParameter} which must
+//     *                      have returned {@code true}.
+//     * @param mavContainer  the ModelAndViewContainer for the current request
+//     * @param webRequest    the current request
+//     * @param binderFactory a factory for creating {@link WebDataBinder} instances
+//     * @return the resolved argument value, or {@code null} if not resolvable
+//     * @throws Exception in case of errors with the preparation of argument values
+//     */
+//    //@Override
+//    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+//        String jsonBody = this.getRequestBody(webRequest);
+//
+//        return null;
+//    }
+//
+//    private String getRequestBody(NativeWebRequest webRequest) {
+//        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+//
+//        String jsonRequestBody = (String) webRequest.getAttribute("JSON_REQUEST_BODY", 0);
+//        if (jsonRequestBody == null) {
+//            try {
+//                jsonRequestBody = IOUtils.toString(servletRequest.getReader());
+//                webRequest.setAttribute("JSON_REQUEST_BODY", jsonRequestBody, 0);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//
+//
+//        return null;
+//    }
 }
--- core/src/main/java/com/runyuanj/core/web/advice/MiddleExceptionHandlerAdvice.java
+++ core/src/main/java/com/runyuanj/core/web/advice/MiddleExceptionHandlerAdvice.java
@@ -6,53 +6,47 @@ import com.runyuanj.common.response.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MultipartException;
 
+import static com.runyuanj.common.exception.type.DbErrorType.DUPLICATE_PRIMARY_KEY;
 import static com.runyuanj.common.exception.type.NullPointerErrorType.NULL_POINTER;
+import static com.runyuanj.common.exception.type.SystemErrorType.PARAM_INVALID;
+import static com.runyuanj.common.exception.type.SystemErrorType.UPLOAD_FILE_SIZE_LIMIT;
 
-// @ControllerAdvice
-// @Order(100)
+@ControllerAdvice
 @Slf4j
 public class MiddleExceptionHandlerAdvice {
 
     @ExceptionHandler(value = IllegalArgumentException.class)
     public Result illegalArgumentException(IllegalArgumentException e) {
         log.error("IllegalArgumentException: {}", e.getMessage());
-        return Result.fail(SystemErrorType.PARAM_INVALID);
-    }
-
-    @ExceptionHandler(value = {MissingServletRequestParameterException.class})
-    public Result missingServletRequestParameterException(MissingServletRequestParameterException ex) {
-        log.error("missing servlet request parameter exception:{}", ex.getMessage());
-        return Result.fail(SystemErrorType.PARAM_INVALID);
+        return Result.fail(PARAM_INVALID);
     }
 
     @ExceptionHandler(value = {MultipartException.class})
     public Result uploadFileLimitException(MultipartException ex) {
         log.error("upload file size limit:{}", ex.getMessage());
-        return Result.fail(SystemErrorType.UPLOAD_FILE_SIZE_LIMIT);
+        return Result.fail(UPLOAD_FILE_SIZE_LIMIT);
     }
 
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
     public Result serviceException(MethodArgumentNotValidException ex) {
         log.error("service exception:{}", ex.getMessage());
-        return Result.fail(SystemErrorType.PARAM_INVALID, null, ex.getBindingResult().getFieldError().getDefaultMessage());
+        return Result.fail(PARAM_INVALID, null, ex.getBindingResult().getFieldError().getDefaultMessage());
     }
 
     @ExceptionHandler(value = {DuplicateKeyException.class})
     public Result duplicateKeyException(DuplicateKeyException ex) {
         log.error("primary key duplication exception:{}", ex.getMessage());
-        return Result.fail(DbErrorType.DUPLICATE_PRIMARY_KEY);
+        return Result.fail(DUPLICATE_PRIMARY_KEY);
     }
-
 
     @ExceptionHandler(value = {NullPointerException.class})
     public Result baseException(NullPointerException ex) {
         log.error("NullPointerException {}", ex.getMessage());
         return Result.fail(NULL_POINTER);
     }
-
 
 }
